4095591fba63d6dcc6e8266427b952e0fa3d4615
mvladex <mvladex@gmail.com>
2022-05-28 12:42:34 -0500

    base de datos

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
 from flask import Flask,render_template
+import pyrebase
+
+
+
+
+
+#variable de configuracion
+config={
+    "apiKey": "AIzaSyDJi3FX5e5qwuy72P5RbuVMm0O5-rHyxlM",
+    "authDomain": "boxfit-153ae.firebaseapp.com",
+    "databaseURL": "https://boxfit-153ae-default-rtdb.firebaseio.com",
+    "projectId": "boxfit-153ae",
+    "storageBucket": "boxfit-153ae.appspot.com",
+    "messagingSenderId": "331502980923",
+    "appId": "1:331502980923:web:5b213d4742c9d041f2711e"
+}
+
+firebase=pyrebase.initialize_app(config)
+db=firebase.database()
 
 
 app = Flask(__name__)
