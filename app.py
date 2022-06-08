c36855e0b23a0dda9294422410a4519c086acc63
Ignaciocruga <ignaciodezentt@gmail.com>
2022-06-07 19:29:56 -0600

    se puede hacer registros y se guardan en fb

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import pyrebase
-
+from flask import url_for
+import json
+from models.usuario import usuario
 
 
 
 
 #variable de configuracion
 config={
-    "apiKey": "AIzaSyDJi3FX5e5qwuy72P5RbuVMm0O5-rHyxlM",
-    "authDomain": "boxfit-153ae.firebaseapp.com",
-    "databaseURL": "https://boxfit-153ae-default-rtdb.firebaseio.com",
-    "projectId": "boxfit-153ae",
-    "storageBucket": "boxfit-153ae.appspot.com",
-    "messagingSenderId": "331502980923",
-    "appId": "1:331502980923:web:5b213d4742c9d041f2711e"
+  "apiKey": "AIzaSyBTY_jIHQE5Khyq0__BNhxylrApmYqSMZM",
+  "authDomain": "boxfit-d57c1.firebaseapp.com",
+  "databaseURL": "https://boxfit-d57c1-default-rtdb.firebaseio.com",
+  "projectId": "boxfit-d57c1",
+  "storageBucket": "boxfit-d57c1.appspot.com",
+  "messagingSenderId": "730838439699",
+  "appId": "1:730838439699:web:bce749eb12d63a7ee3ace6",
 }
 
 firebase=pyrebase.initialize_app(config)
@@ -22,12 +24,50 @@ db=firebase.database()
 
 app = Flask(__name__)
 
+@app.route('/prueba')
+def prueba():
+    lista_clientes=db.child("clientes").get().val()
+
+    return render_template("tabla.html",elementos_clientes=lista_clientes.values())
 
 @app.route('/')
 def getIndex():  # put application's code here
+    lista=db.child("clientes").get()
+    try:
+        lista_clientes=lista.val()
+        lista_wars=lista_clientes.keys()
+        lista_war_final=list(lista_wars)
+        return render_template("registro.html",lista_cliente=lista_clientes.values(),lista_war_final=lista_war_final)
+    except:
+        return render_template("registros.html")
+
+#ruta para mostrar formulario de registro
+@app.route('/add')
+def add():
     return render_template("registro.html")
 
-@app.route('/re')
+#guardatos de formulario en fb
+@app.route('/save_data',methods=['POST'])
+def save_data():
+    nombre=request.form.get('nombre')
+    apellido=request.form.get('apellido')
+    direccion=request.form.get('direccion')
+    telefono=request.form.get('telefono')
+    correo=request.form.get('correo')
+    neew_clien=usuario(nombre,apellido,direccion,telefono,correo)
+    submit_form =json.dumps(neew_clien.__dict__)
+
+    moon = json.loads(submit_form)
+    db.child("clientes").push(moon)
+
+    #db.child("clientes").push({"nombre": nombre, "apellido":apellido, "direccion":direccion, "telefono":telefono, "correo":correo })
+    return render_template("registro2.html")
+
+
+
+
+
+@app.route('/re',methods=['POST'])
 def re():  # put application's code here
     return render_template("registro2.html")
 
---
+++ models/usuario.py
@@ -0,0 +1,7 @@
+class usuario():
+    def __init__(self,nombre,apellido,direccion,telefono,correo):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.direccion=direccion
+        self.telefono=telefono
+        self.correo=correo
